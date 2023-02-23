38c26d2861f8c02586c8caa235983e6a3fd3740a
marcobori <55789764+marcobori@users.noreply.github.com>
2023-02-23 16:12:49 +0100

    final

--- u1.m
+++ u1.m
@@ -2,11 +2,11 @@ clear;
 defpops = [-500 -500 -500 -500 -500 -500 -500 -500 -500 -500
             500 500 500 500 500 500 500 500 500 500 ];
 populazion = 100;
-generazion = 300;
+generazion = 500;
 
 oldpops = genrpop(populazion,defpops);
-vyber = [populazion*0.1 populazion*0.01 populazion*0.01];
-vybersize = 70;
+vyber = [populazion*0.10 populazion*0.03 populazion*0.01];
+vybersize = populazion-vyber;
     figure, hold on;
     xlabel('generation');
     ylabel('fitness');
@@ -24,13 +24,16 @@ for i = 1:generazion
     bestnew = selbest(oldpops,testfn3(oldpops),1);
     finale = [finale testfn3(bestnew)];
     %plot(i,testfn3(bestnew),'-r.',markersize=5);
-    newpops=selbest(oldpops,testfn3(oldpops),vyber);
-    randpops=selrand(oldpops,testfn3(oldpops),populazion-vybersize);
+
+    newpops=seltourn(oldpops,testfn3(oldpops),vybersize);
+    randpops=selbest(oldpops,testfn3(oldpops),vyber);
+    newpops=crossov(newpops,1,0);
     newpops = vertcat(newpops,randpops);
-    newpops=crossov(newpops,1,1);
-    newpops=mutx(newpops,0.5,defpops);
+    newpops=mutx(newpops,0.1,defpops);
+    newpops=muta(newpops,0.1,0.05*[1 1 1 1 1 1 1 1 1 1],defpops);    
     oldpops = newpops;
     %bestold = bestnew;
 end 
 final = selbest(newpops,testfn3(newpops),1)
+disp(testfn3(final));
 plot(icka,finale,'.-r');
