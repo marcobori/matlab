e525560de933268b077d4af735d84c5bf5e4a9da
marcobori <55789764+marcobori@users.noreply.github.com>
2023-03-08 18:57:37 +0100

    Add files via upload

--- u2.m
+++ u2.m
@@ -2,7 +2,8 @@ clear
 B=[0,0; 77,68; 12,75; 32,17; 51,64; 20,19; 72,87; 80,37; 35,82;2,15; 18,90; 33,50; 85,52; 97,27; 37,67; 20,82; 49,0; 62,14; 7,60;100,100];
 popsize = 100;
 genum = 500;
-for h = 1:5
+%tiledlayout(3,4)
+for h = 1:10
 for i=1:popsize
     PermPop(i,:) = randperm(18);
 end
@@ -15,10 +16,10 @@ for gen = 1 : genum
     bestpop = selbest(PermPop,fitness,[3,1,1]);
     oldpop = seltourn(PermPop,fitness,10);
     newpop = [seltourn(PermPop,fitness,popsize-20);bestpop];
-    newpop = swapgen(newpop,0.1);
-    newpop = swappart(newpop,0.1);
     newpop = invord(newpop,0.3);
     newpop = crosord(newpop,0);
+    newpop = swapgen(newpop,0.1);
+    newpop = swappart(newpop,0.1);
     PermPop = [bestpop;newpop;oldpop];
 end
 res = [1,bestpop(1,:),20];
@@ -29,14 +30,16 @@ plot(graf);
 figure(2);
 hold on;
 X=[];Y=[];
+
 for i=1:20
     x=B(res(i),1); y=B(res(i),2);
-    plot(x,y,'mo');
+    plot(x,y,'ko',LineWidth=2);
     X=[X,x]; Y=[Y,y];
 end
-line(X,Y); 
+line(X,Y,'Linewidth',2,'color',round(rand(1,3),3)); 
 
 minim(h)=min(fitness);
+nexttile
 end
 disp('min:');disp(min(minim));
 
