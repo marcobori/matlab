edd13d1593f80a4d7fc341bfa3e6651c1d19c2da
marcobori <55789764+marcobori@users.noreply.github.com>
2023-04-13 13:29:48 +0200

    Add files via upload

--- u6.m
+++ u6.m
@@ -1,10 +1,10 @@
+close all
 clear
 load CTGdata.mat 
 NDATA = transpose(NDATA);
 arrtr = [];
 arrts = [];
 best =[];
-A = [];
 target = zeros(3,size(typ_ochorenia,1));      
 for xd = 1:10
 for i=1:size(typ_ochorenia,1)
@@ -24,7 +24,6 @@ vystup = target;
 arr = [];
 
 net = patternnet(20);
-A = [A;net];
 
 net.divideFcn = 'dividerand';
 
@@ -32,17 +31,21 @@ net.divideParam.trainRatio = 0.6;
 net.divideParam.valRatio = 0;
 net.divideParam.testRatio = 0.4;
 
-net.trainParam.goal = 0.001;      
-net.trainParam.epochs = 200;     
-net.trainParam.min_grad = 1e-20;  
+net.trainParam.goal = 0.01;      
+net.trainParam.epochs = 300;     
+net.trainParam.min_grad = 1e-10;  
 net.performFcn = 'msereg';
 % net.performFcn = 'mse';
 % net.performFcn = 'sse';
 % net.performFcn = 'mae';
 
 [net,tr] = train(net,NDATA,vystup);
-outputs = net(NDATA);
 
+%%zakomentuj
+figure, plotperform(tr);
+subtitle(xd);
+
+outputs = net(NDATA);
 outputstrain=net(NDATA(:,tr.trainInd));
 outputstest=net(NDATA(:,tr.testInd));
 trainTargets=target(:,tr.trainInd);
@@ -52,14 +55,17 @@ testTargets=target(:,tr.testInd);
 [c_train,cm_train] = confusion(trainTargets,outputstrain);
 [c_test,cm_test] = confusion(testTargets,outputstest);
 
-
+%%zakomentuj
+figure, plotconfusion(vystup,outputs);
+subtitle(xd);
 fprintf('all:%d %.4f%% train: %.4f%% test: %.4f%% \n',xd, 100*(1-c), 100*(1-c_train), 100*(1-c_test));
 best = [best;100*(1-c)];
 arrtr = [arrtr;100*(1-c_train)];
 arrts = [arrts;100*(1-c_test)];
 end
 
-[val,idx] = max(best);
+[val,idx] = max(best)
+fprintf('best index %d\n',idx);
 
 trmin = min(arrtr);
 trmax = max(arrtr);
